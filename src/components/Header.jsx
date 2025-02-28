703c426ea09b14c3e1b7837583b2ffba9f4635de
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2025-02-28 14:41:09 -0600

    Displaying if cart is empty or not depending on the lenght

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,10 @@ function Header({ cart }) {
               <img className="img-fluid" src="./public/img/carrito.png" alt="imagen carrito" />
 
               <div id="carrito" className="bg-white p-3">
-                <p className="text-center">El carrito esta vacio</p>
+                {cart.length === 0 ? (
+                  <p className="text-center">El carrito esta vacio</p>
+                ) : (
+
                 <table className="w-100 table">
                   <thead>
                     <tr>
@@ -64,6 +67,7 @@ function Header({ cart }) {
                     ))}
                   </tbody>
                 </table>
+                )}
 
                 <p className="text-end">Total pagar: <span className="fw-bold">$899</span></p>
                 <button className="btn btn-dark w-100 mt-3 p-2">Vaciar Carrito</button>
